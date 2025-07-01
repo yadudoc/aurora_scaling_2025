77097ae7bcb23b6f77a68fb03c94f05ef4b03e21
Yadu Nand Babuji <yadunand@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
2025-07-01 16:27:31 +0000

    Adding data from 2048 node runs

--- experiment_runs/submit_latency_512.sh
+++ experiment_runs/submit_latency_512.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #PBS -l select=512
-#PBS -l walltime=02:00:00
+#PBS -l walltime=01:00:00
 #PBS -q prod
 #PBS -A workflow_scaling
 #PBS -l filesystems=home:flare
@@ -16,19 +16,19 @@ ROOT=/home/yadunand/aurora_scaling_2025/experiment_runs
 LOG="$ROOT/perf_latency_sleep.logs"
 
 
-EXP_RECORD="$ROOT/experiment_task_efficiency_32_512.json"
+EXP_RECORD="$ROOT/experiment_task_efficiency_32_512.prefetch0.json"
 EXP_CODE="$ROOT/test_latency.py"
 cd $ROOT
 export PYTHONPATH=$ROOT:$PYTHONPATH
 
 
-for config in config_variable_workers_no_logging_prefetch
+for config in config_variable_workers_no_logging_no_prefetch
 do
     echo "Running experiments with $config"
     echo "==============================================="
-    for NODES in 512 256 128 64 32
+    for NODES in 512 256
     do
-        for SLEEP_DUR in 10 100
+        for SLEEP_DUR in 10
         do
             for WORKERS in 12
             do
@@ -36,7 +36,32 @@ do
                 export NODES_IN_JOB=$NODES
                 export MAX_WORKERS_PER_NODE=$WORKERS
                 python3 $EXP_CODE --fileconfig=$config -s $SLEEP_DUR \
-                        --count_per_worker=10 \
+                        --count_per_worker=5 \
+                        --nodes=$NODES \
+                        --workers_per_node=$WORKERS \
+                        --latency \
+                        --output_record=$EXP_RECORD >> $LOG
+            done
+        done
+    done
+    echo "==============================================="
+done
+
+for config in config_variable_workers_no_logging_no_prefetch
+do
+    echo "Running experiments with $config"
+    echo "==============================================="
+    for NODES in 512 256
+    do
+        for SLEEP_DUR in 100
+        do
+            for WORKERS in 12
+            do
+                echo "Running experiment with NODES=$NODES, SLEEP_DUR=$SLEEP_DUR, and WORKERS=$WORKERS"
+                export NODES_IN_JOB=$NODES
+                export MAX_WORKERS_PER_NODE=$WORKERS
+                python3 $EXP_CODE --fileconfig=$config -s $SLEEP_DUR \
+                        --count_per_worker=5 \
                         --nodes=$NODES \
                         --workers_per_node=$WORKERS \
                         --latency \
